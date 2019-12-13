7312fd4c70d8deeda531f6041beff2afea5e689b
Vivek <b.vivek065@gmail.com>
2019-12-12 20:18:09 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is my first app with react. - Vivek</h1>
+    </div>
+  );
+}
+
+export default App;
